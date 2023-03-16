8b9770a92a7d2ee938b46db28758703522e979b1
Narathipp <narathip.pi@kkumail.com>
2023-03-17 00:08:12 +0700

    toto file

--- src/main/java/nl/tudelft/jpacman/ui/BoardPanel.java
+++ src/main/java/nl/tudelft/jpacman/ui/BoardPanel.java
@@ -1,22 +1,26 @@
 package nl.tudelft.jpacman.ui;
 
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
+import java.awt.*;
+import java.util.Map;
 
-import javax.swing.JPanel;
+import javax.swing.*;
 
 import nl.tudelft.jpacman.board.Board;
 import nl.tudelft.jpacman.board.Square;
 import nl.tudelft.jpacman.board.Unit;
 import nl.tudelft.jpacman.game.Game;
 
+
+import javax.swing.JLayeredPane;
+import javax.swing.JFrame;
+
 /**
  * Panel displaying a game.
  *
- * @author Jeroen Roosen 
+ * @author Jeroen Roosen
  *
  */
+
 class BoardPanel extends JPanel {
 
     /**
@@ -27,13 +31,14 @@ class BoardPanel extends JPanel {
     /**
      * The background colour of the board.
      */
-    private static final Color BACKGROUND_COLOR = Color.BLACK;
+    private static final Color BACKGROUND_COLOR = Color.BLUE;
 
     /**
      * The size (in pixels) of a square on the board. The initial size of this
      * panel will scale to fit a board with square of this size.
      */
     private static final int SQUARE_SIZE = 16;
+
 
     /**
      * The game to display.
@@ -46,7 +51,8 @@ class BoardPanel extends JPanel {
      * @param game
      *            The game to display.
      */
-    BoardPanel(Game game) {
+
+    BoardPanel(Game game,final Map<String, Action> buttons,final JFrame parent) {
         super();
         assert game != null;
         this.game = game;
@@ -59,13 +65,87 @@ class BoardPanel extends JPanel {
         Dimension size = new Dimension(w, h);
         setMinimumSize(size);
         setPreferredSize(size);
+
+        assert buttons != null;
+        assert parent != null;
+
+        ImageIcon startIcon = new ImageIcon("src/main/resources/button/start_btn.png");
+        Image startImg = startIcon.getImage().getScaledInstance(100, 25, Image.SCALE_SMOOTH);
+        ImageIcon startBtn = new ImageIcon(startImg);
+
+        ImageIcon stopIcon = new ImageIcon("src/main/resources/button/pause_btn.png");
+        Image stopImg = stopIcon.getImage().getScaledInstance(100, 25, Image.SCALE_SMOOTH);
+        ImageIcon stopBtn = new ImageIcon(stopImg);
+
+        ImageIcon exitIcon = new ImageIcon("src/main/resources/button/exit_btn.png");
+        Image exitImg = exitIcon.getImage().getScaledInstance(100, 25, Image.SCALE_SMOOTH);
+        ImageIcon exitBtn = new ImageIcon(exitImg);
+
+        for (final String caption : buttons.keySet()) {
+
+            JButton Startbutton = new JButton(caption);
+            JButton Stopbutton = new JButton(caption);
+            JButton Exitbutton = new JButton(caption);
+
+
+            Startbutton.addActionListener(e -> {
+                buttons.get(caption).doAction();
+                parent.requestFocusInWindow();
+            });
+
+            Stopbutton.addActionListener(e -> {
+                buttons.get(caption).doAction();
+                parent.requestFocusInWindow();
+            });
+
+            Exitbutton.addActionListener(e -> {
+                buttons.get(caption).doAction();
+                parent.requestFocusInWindow();
+            });
+
+
+            if (Startbutton.getText().equals("Start")) {
+                Startbutton.setIcon(startBtn);
+                Startbutton.setOpaque(false);
+                Startbutton.setBorder(null);
+                Startbutton.setBorderPainted(false);
+                Startbutton.setContentAreaFilled(false);
+                Startbutton.setText(" ");
+                setComponentZOrder(Startbutton, 0);
+                add(Startbutton, BorderLayout.CENTER);
+                Startbutton.setBounds(80, 670, 200, 50);
+
+
+            } else if (Stopbutton.getText().equals("Stop")) {
+                Stopbutton.setIcon(stopBtn);
+                Stopbutton.setOpaque(false);
+                Stopbutton.setBorder(null);
+                Stopbutton.setBorderPainted(false);
+                Stopbutton.setContentAreaFilled(false);
+                Stopbutton.setText(" ");
+                add(Stopbutton);
+                Stopbutton.setBounds(100, 670, 200, 50);
+
+            }else if (Exitbutton.getText().equals("Exit")) {
+                Exitbutton.setIcon(exitBtn);
+                Exitbutton.setOpaque(false);
+                Exitbutton.setBorder(null);
+                Exitbutton.setBorderPainted(false);
+                Exitbutton.setContentAreaFilled(false);
+                Exitbutton.setText(" ");
+                add(Exitbutton);
+                Exitbutton.setBounds(120, 670, 200, 50);
+            }
+        }
     }
 
     @Override
     public void paint(Graphics g) {
         assert g != null;
         render(game.getLevel().getBoard(), g, getSize());
+
     }
+
 
     /**
      * Renders the board on the given graphics context to the given dimensions.
@@ -82,6 +162,7 @@ class BoardPanel extends JPanel {
         int cellH = window.height / board.getHeight();
 
         graphics.setColor(BACKGROUND_COLOR);
+        //graphics.setColor(BACKGROUND_COLOR);
         graphics.fillRect(0, 0, window.width, window.height);
 
         for (int y = 0; y < board.getHeight(); y++) {
--- src/main/java/nl/tudelft/jpacman/ui/PacManUI.java
+++ src/main/java/nl/tudelft/jpacman/ui/PacManUI.java
@@ -1,15 +1,12 @@
 package nl.tudelft.jpacman.ui;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
+import java.awt.*;
 import java.util.Map;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.WindowConstants;
+import javax.swing.*;
 
 import nl.tudelft.jpacman.game.Game;
 import nl.tudelft.jpacman.ui.ScorePanel.ScoreFormatter;
@@ -30,92 +27,93 @@ import nl.tudelft.jpacman.ui.ScorePanel.ScoreFormatter;
  */
 public class PacManUI extends JFrame {
 
-	/**
-	 * Default serialisation UID.
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     * Default serialisation UID.
+     */
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * The desired frame rate interval for the graphics in milliseconds, 40
-	 * being 25 fps.
-	 */
-	private static final int FRAME_INTERVAL = 40;
+    /**
+     * The desired frame rate interval for the graphics in milliseconds, 40
+     * being 25 fps.
+     */
+    private static final int FRAME_INTERVAL = 40;
 
-	/**
-	 * The panel displaying the player scores.
-	 */
-	private final ScorePanel scorePanel;
+    /**
+     * The panel displaying the player scores.
+     */
+    private final ScorePanel scorePanel;
 
-	/**
-	 * The panel displaying the game.
-	 */
-	private final BoardPanel boardPanel;
+    /**
+     * The panel displaying the game.
+     */
+    private final BoardPanel boardPanel;
 
-	/**
-	 * Creates a new UI for a JPacman game.
-	 *
-	 * @param game
-	 *                       The game to play.
-	 * @param buttons
-	 *                       The map of caption-to-action entries that will appear
-	 *                       as
-	 *                       buttons on the interface.
-	 * @param keyMappings
-	 *                       The map of keyCode-to-action entries that will be added
-	 *                       as key
-	 *                       listeners to the interface.
-	 * @param scoreFormatter
-	 *                       The formatter used to display the current score.
-	 */
+    /**
+     * Creates a new UI for a JPacman game.
+     *
+     * @param game
+     *                       The game to play.
+     * @param buttons
+     *                       The map of caption-to-action entries that will appear
+     *                       as
+     *                       buttons on the interface.
+     * @param keyMappings
+     *                       The map of keyCode-to-action entries that will be added
+     *                       as key
+     *                       listeners to the interface.
+     * @param scoreFormatter
+     *                       The formatter used to display the current score.
+     */
 
-	public PacManUI(final Game game, final Map<String, Action> buttons,
-			final Map<Integer, Action> keyMappings,
-			ScoreFormatter scoreFormatter) {
-		super("JPacman");
-		assert game != null;
-		assert buttons != null;
-		assert keyMappings != null;
+    public PacManUI(final Game game, final Map<String, Action> buttons,
+                    final Map<Integer, Action> keyMappings,
+                    ScoreFormatter scoreFormatter) {
+        super("JPacman");
+        assert game != null;
+        assert buttons != null;
+        assert keyMappings != null;
 
-		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-		PacKeyListener keys = new PacKeyListener(keyMappings);
-		addKeyListener(keys);
+        PacKeyListener keys = new PacKeyListener(keyMappings);
+        addKeyListener(keys);
 
-		JPanel buttonPanel = new ButtonPanel(buttons, this);
+        //JPanel buttonPanel = new BoardPanel(buttons, this);;
 
-		scorePanel = new ScorePanel(game.getPlayers());
-		if (scoreFormatter != null) {
-			scorePanel.setScoreFormatter(scoreFormatter);
-		}
+        scorePanel = new ScorePanel(game.getPlayers());
+        if (scoreFormatter != null) {
+            scorePanel.setScoreFormatter(scoreFormatter);
+        }
 
-		boardPanel = new BoardPanel(game);
+        boardPanel = new BoardPanel(game,buttons, this);
 
-		Container contentPanel = getContentPane();
-		contentPanel.setLayout(new BorderLayout());
-		contentPanel.add(buttonPanel, BorderLayout.SOUTH);
-		contentPanel.add(scorePanel, BorderLayout.NORTH);
-		contentPanel.add(boardPanel, BorderLayout.CENTER);
+        Container contentPanel = getContentPane();
+        contentPanel.setLayout(new BorderLayout());
+        //contentPanel.add(buttonPanel, BorderLayout.SOUTH);
+        contentPanel.add(scorePanel, BorderLayout.NORTH);
+        contentPanel.add(boardPanel, BorderLayout.CENTER);
+        //lp.add(BoardPanel.game,new Integer(3));
 
-		pack();
-	}
+        pack();
+    }
 
-	/**
-	 * Starts the "engine", the thread that redraws the interface at set
-	 * intervals.
-	 */
-	public void start() {
-		setSize(600, 800);
-		setVisible(true);
-        setResizable(false);
-		ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
-		service.scheduleAtFixedRate(this::nextFrame, 0, FRAME_INTERVAL, TimeUnit.MILLISECONDS);
-	}
+    /**
+     * Starts the "engine", the thread that redraws the interface at set
+     * intervals.
+     */
+    public void start() {
+        setSize(600, 800);
+        setVisible(true);
+        setResizable(true);
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        service.scheduleAtFixedRate(this::nextFrame, 0, FRAME_INTERVAL, TimeUnit.MILLISECONDS);
+    }
 
-	/**
-	 * Draws the next frame, i.e. refreshes the scores and game.
-	 */
-	private void nextFrame() {
-		boardPanel.repaint();
-		scorePanel.refresh();
-	}
+    /**
+     * Draws the next frame, i.e. refreshes the scores and game.
+     */
+    private void nextFrame() {
+        boardPanel.repaint();
+        scorePanel.refresh();
+    }
 }
