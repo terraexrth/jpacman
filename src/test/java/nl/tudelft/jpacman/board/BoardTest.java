779bbe4a6c64e6fdbeef1a25886bf940c949f1b5
atsadawutcs <atsadawut.h@kkumail.com>
2023-03-20 22:52:45 +0700

    Unit Test: Test Board (Score & Pallet) and UI

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -1,10 +1,9 @@
-package nl.tudelft.jpacman;
+package nl.tudelft.jpacman.board;
 
-import jdk.jfr.Description;
+import nl.tudelft.jpacman.Launcher;
 import nl.tudelft.jpacman.board.Direction;
 import nl.tudelft.jpacman.game.Game;
 import nl.tudelft.jpacman.level.Player;
-import nl.tudelft.jpacman.ui.MainMenu;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -14,20 +13,20 @@ import java.util.Random;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class LauncherTestTest {
+public class BoardTest {
     private Launcher launcher;
 
     /**
      * Launch the user interface.
      */
-    @BeforeEach
+    /*@BeforeEach
     void setUpPacman() {
         //mainmenu = new MainMenu();
         launcher = new Launcher();
         launcher.withMapFile("/board1test.txt");
         launcher.launch();
         //mainmenu.openMap();
-    }
+    }*/
 
     /**
      * Quit the user interface when we're done.
@@ -44,38 +43,7 @@ public class LauncherTestTest {
      *
      * @throws InterruptedException Since we're sleeping in this test.
      */
-
-    @DisplayName("T01 : Game Should Start")
-    @Test
-    void GameStartTest() throws InterruptedException{
-        Game game = launcher.getGame();
-        Player player = game.getPlayers().get(0);
-        // start cleanly.
-        assertThat(game.isInProgress()).isFalse();
-        game.start();
-        assertThat(game.isInProgress()).isTrue();
-        assertThat(player.getScore()).isZero();
-        Thread.sleep(1000);
-    }
-
-
-    @DisplayName("T02 : Packman should eat all padlet (Padlet remaining = 0)")
-    @Test
-    void GetAllPadletTest() throws InterruptedException {
-        Game game = launcher.getGame();
-
-        Player player = game.getPlayers().get(0);
-        //GameEndTest
-        game.start();
-        int total = game.getLevel().remainingPellets();
-        while(game.getLevel().remainingPellets() != 0){
-            game.move(player,getRandomDirection());
-        }
-        Thread.sleep(2000);
-        assertThat(player.getScore()).isEqualTo(total*10);
-        game.stop();
-    }
-
+    
     @Test
     void MainmenuStartTest() throws InterruptedException{
 
@@ -86,27 +54,7 @@ public class LauncherTestTest {
         return Direction.values()[new Random().nextInt(Direction.values().length)];
     }
 
-
-    @DisplayName("T03 : Tutorial")
-    @Test
-    void TutorialTest() throws InterruptedException{
-        launcher = new Launcher();
-        launcher.withMapFile("/boardtutorialtest.txt");
-        launcher.launch();
-        Game game = launcher.getGame();
-
-        Player player = game.getPlayers().get(0);
-        //GameEndTest
-        game.start();
-        int total = game.getLevel().remainingPellets();
-        while(game.getLevel().remainingPellets() != 0){
-            game.move(player,getRandomDirection());
-        }
-        Thread.sleep(2000);
-        assertThat(player.getScore()).isEqualTo(total*10);
-        game.stop();
-    }
-
+    @DisplayName("TC01: Score Board 1")
     @Test
     void Board1Test() throws InterruptedException{
         launcher = new Launcher();
@@ -125,7 +73,27 @@ public class LauncherTestTest {
         assertThat(player.getScore()).isEqualTo(total*10);
         game.stop();
     }
+    @DisplayName("TC02 : Pallet after pacman ate all Board 1")
+    @Test
+    void GetPalletAfterPacmanAteAll1() throws InterruptedException {
+        launcher = new Launcher();
+        launcher.withMapFile("/board1test.txt");
+        launcher.launch();
+        Game game = launcher.getGame();
 
+        Player player = game.getPlayers().get(0);
+        game.start();
+        int total = 0;
+        while(game.getLevel().remainingPellets() != 0){
+            game.move(player,getRandomDirection());
+            total = game.getLevel().remainingPellets();
+        }
+        Thread.sleep(2000);
+        assertThat(total).isEqualTo(0);
+        game.stop();
+    }
+
+    @DisplayName("TC03: Score Board 2")
     @Test
     void Board2Test() throws InterruptedException{
         launcher = new Launcher();
@@ -145,6 +113,27 @@ public class LauncherTestTest {
         game.stop();
     }
 
+    @DisplayName("T04 : Pallet after pacman ate all Board 2")
+    @Test
+    void GetPalletAfterPacmanAteAll2() throws InterruptedException {
+        launcher = new Launcher();
+        launcher.withMapFile("/board2test.txt");
+        launcher.launch();
+        Game game = launcher.getGame();
+
+        Player player = game.getPlayers().get(0);
+        game.start();
+        int total = 0;
+        while(game.getLevel().remainingPellets() != 0){
+            game.move(player,getRandomDirection());
+            total = game.getLevel().remainingPellets();
+        }
+        Thread.sleep(2000);
+        assertThat(total).isEqualTo(0);
+        game.stop();
+    }
+
+    @DisplayName("TC05: Score Board 3")
     @Test
     void Board3Test() throws InterruptedException{
         launcher = new Launcher();
@@ -163,7 +152,27 @@ public class LauncherTestTest {
         assertThat(player.getScore()).isEqualTo(total*10);
         game.stop();
     }
+    @DisplayName("T06 : Pallet after pacman ate all Board 3")
+    @Test
+    void GetPalletAfterPacmanAteAll3() throws InterruptedException {
+        launcher = new Launcher();
+        launcher.withMapFile("/board3test.txt");
+        launcher.launch();
+        Game game = launcher.getGame();
 
+        Player player = game.getPlayers().get(0);
+        game.start();
+        int total = 0;
+        while(game.getLevel().remainingPellets() != 0){
+            game.move(player,getRandomDirection());
+            total = game.getLevel().remainingPellets();
+        }
+        Thread.sleep(2000);
+        assertThat(total).isEqualTo(0);
+        game.stop();
+    }
+
+    @DisplayName("TC07: Score Board 4")
     @Test
     void Board4Test() throws InterruptedException{
         launcher = new Launcher();
@@ -182,7 +191,27 @@ public class LauncherTestTest {
         assertThat(player.getScore()).isEqualTo(total*10);
         game.stop();
     }
+    @DisplayName("T08 : Pallet after pacman ate all Board 4")
+    @Test
+    void GetPalletAfterPacmanAteAll4() throws InterruptedException {
+        launcher = new Launcher();
+        launcher.withMapFile("/board4test.txt");
+        launcher.launch();
+        Game game = launcher.getGame();
 
+        Player player = game.getPlayers().get(0);
+        game.start();
+        int total = 0;
+        while(game.getLevel().remainingPellets() != 0){
+            game.move(player,getRandomDirection());
+            total = game.getLevel().remainingPellets();
+        }
+        Thread.sleep(2000);
+        assertThat(total).isEqualTo(0);
+        game.stop();
+    }
+
+    @DisplayName("TC09: Score Board 5")
     @Test
     void Board5Test() throws InterruptedException{
         launcher = new Launcher();
@@ -199,6 +228,25 @@ public class LauncherTestTest {
         }
         Thread.sleep(2000);
         assertThat(player.getScore()).isEqualTo(total*10);
+        game.stop();
+    }
+    @DisplayName("TC10 : Pallet after pacman ate all Board 5")
+    @Test
+    void GetPalletAfterPacmanAteAll5() throws InterruptedException {
+        launcher = new Launcher();
+        launcher.withMapFile("/board5test.txt");
+        launcher.launch();
+        Game game = launcher.getGame();
+
+        Player player = game.getPlayers().get(0);
+        game.start();
+        int total = 0;
+        while(game.getLevel().remainingPellets() != 0){
+            game.move(player,getRandomDirection());
+            total = game.getLevel().remainingPellets();
+        }
+        Thread.sleep(2000);
+        assertThat(total).isEqualTo(0);
         game.stop();
     }
 }
