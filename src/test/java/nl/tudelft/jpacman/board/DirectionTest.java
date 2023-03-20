7552f5cbb1c03d353cb9f4a801a84c8ea740cb57
atsadawutcs <atsadawut.h@kkumail.com>
2023-03-20 21:16:27 +0700

    Unit Test: Test Direction Move 1 Block (up,down,left,right) (DirectionTest.java)

--- src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
+++ src/test/java/nl/tudelft/jpacman/board/DirectionTest.java
@@ -2,6 +2,7 @@ package nl.tudelft.jpacman.board;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 /**
@@ -14,9 +15,28 @@ public class DirectionTest {
     /**
      * Do we get the correct delta when moving north?
      */
+    @DisplayName("TC01: Move To North 1 Block")
     @Test
     void testNorth() {
         Direction north = Direction.valueOf("NORTH");
         assertThat(north.getDeltaY()).isEqualTo(-1);
+    }
+    @DisplayName("TC02: Move To South 1 Block")
+    @Test
+    void testSouth() {
+        Direction north = Direction.valueOf("SOUTH");
+        assertThat(north.getDeltaY()).isEqualTo(1);
+    }
+    @DisplayName("TC03: Move To West 1 Block")
+    @Test
+    void testWest() {
+        Direction north = Direction.valueOf("WEST");
+        assertThat(north.getDeltaX()).isEqualTo(-1);
+    }
+    @DisplayName("TC04: Move To East 1 Block")
+    @Test
+    void testEast() {
+        Direction north = Direction.valueOf("EAST");
+        assertThat(north.getDeltaX()).isEqualTo(1);
     }
 }
